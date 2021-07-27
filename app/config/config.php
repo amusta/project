c573241824e45e2894e4ee018728a1ca3f926762
Ana Mustafov <ana-mustafov@hotmail.com>
2021-07-27 16:04:19 +0200

    Created MVC

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'project');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://project.test');
+
+define('SITENAME', 'Project');
